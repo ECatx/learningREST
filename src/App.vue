59e67a0af0bf19c9fb6ecf71fa5250b5b82e4763
Eric Castellanos <eric@ericcastellanos.com>
2021-05-03 01:53:43 -0500

    testing styles to fit in layout for project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="w-72 h-72 bg-warmGray-400">
-    <div class="w-320 h-320">
-         <img :src="cats.url">
+  <div class="w-72 h-120 bg-warmGray-400 mx-auto object-center items-center">
+    <div class="object-fill"> 
+         <img :src="cats.url" class="w-72 h-72 object-scale-down">
     </div>
-    <div class="flex mx-auto flex-row">
-      <label>Test</label>
-      <input class="flex" :value=cats.url :disabled=1>
-      <button @click="getCat">Get new Cat</button>
+      <button  class="bg-blue-200 rounded shadow-lg flex mx-auto items-center" @click="getCat">Get new Cat</button>
+    <div class="flex flex-row pt-2">
+      <label class="pr-1">Test</label>
+      <input class="text-xs w-full" :value=cats.url :disabled=1>
     </div>
   </div>
 </template>
